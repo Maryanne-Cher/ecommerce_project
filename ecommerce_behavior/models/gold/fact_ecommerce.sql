f941e5d9929589d39b89b893e2a68dbeddff2bee
Maryanne2 <maryanne.kar27@gmail.com>
2025-12-02 15:08:11 -0800

    removed category_id column

---
+++ ecommerce_behavior/models/gold/fact_ecommerce.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        unique_key='event_key'
+    )
+}}
+
+WITH fact_ecommerce AS (
+    SELECT *
+    FROM {{ ref('silver_ecommerce') }}
+),
+
+fact AS (
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY event_date_and_time ASC) AS event_key,
+        event_date_and_time,
+        event_date,
+        event_time_only,
+        event_type,
+        product_id,
+        category_id,
+        category,
+        subcategory,
+        price,
+        user_id,
+        user_session
+    FROM fact_ecommerce
+)
+
+SELECT *
+FROM fact

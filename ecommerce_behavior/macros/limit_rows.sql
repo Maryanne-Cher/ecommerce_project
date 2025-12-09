fbd311d5ae955fdb30bf683d39d08fce254d66e2
Maryanne2 <maryanne.kar27@gmail.com>
2025-12-09 11:19:22 -0800

    deploy dev branch to prod envt

--- ecommerce_behavior/macros/limit_rows.sql
+++ ecommerce_behavior/macros/limit_rows.sql
@@ -1,5 +1,5 @@
 {% macro limit_rows(default_limit=5000000) %}
-    {% if target.name in ['dev', 'ci', 'local'] %}
+    {% if target.name in ['dev', 'ci', 'prod'] %}
         LIMIT {{ default_limit }}
     {% endif %}
 {% endmacro %}
